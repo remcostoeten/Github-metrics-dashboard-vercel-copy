0506397578b7d9b6acf616a13f840c67915f98db
Remco Stoeten <remcostoeten@hotmail.com>
2024-07-31 16:55:00 +0200

    chore: clean up ProjcetList

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -7,14 +7,12 @@ export default function ProjectList() {
   return (
     <div className="flex flex-col grow pb-9 -mt-5 bg-blend-normal max-md:mt-10 max-md:max-w-full space-y-2">
       {projects.map((project, index) => (
-        <>
-          <HoverCard>
-            <ProjectCard repoName="remcostoeten/remcostoeten-all-in-one-dashboard" />
-          </HoverCard>
-        </>
+        <HoverCard key={index}>
+          <ProjectCard repoName={project.repoName} />
+        </HoverCard>
       ))}
       <Link
-        className="mt-9 text-sm  text-blue-500 max-md:max-w-full"
+        className="mt-9 text-sm text-blue-500 max-md:max-w-full"
         target="_blank"
         href="https://github.com/remcostoeten?tab=repositories"
       >
